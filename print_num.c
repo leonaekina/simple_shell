98e085500c20fa52d1e5bf0c5bb8abfe1cc313cc
leonaekina <leonaekina@gmail.com>
2023-04-26 09:31:44 -0700

    Print Number

--- print_num.c
+++ print_num.c
@@ -9,25 +9,24 @@
 
 int print_number(int n)
 {
-        int div;
-        int len;
-        unsigned int num;
+	int div;
+	int len;
+	unsigned int num;
 
-        div = 1;
-        len = 0;
+	div = 1;
+	len = 0;
 
-        num = n;
+	num = n;
 
-        for (; num / div > 9; )
-                div *= 10;
+	for (; num / div > 9; )
+		div *= 10;
 
-        for (; div != 0; )
-        {
-                len += _write_char('0' + num / div);
-                num %= div;
-                div /= 10;
-        }
+	for (; div != 0; )
+	{
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
 
-        return (len);
+	return (len)
 }
-
